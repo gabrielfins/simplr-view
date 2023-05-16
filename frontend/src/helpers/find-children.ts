1716c0297c4d417a35a0bcf72586f6317cd161b9
gabrielfins <contato.gabrielfins@gmail.com>
2023-05-16 03:15:52 -0300

    latest additions

--- frontend/src/helpers/find-children.ts
+++ frontend/src/helpers/find-children.ts
@@ -1,8 +1,15 @@
 import { ReactNode } from 'react';
 import React from 'react';
 
-export function findChildren(children: any, type: Function): Array<ReactNode> {
-  const newChildren: Array<ReactNode> = React.Children.map(children, (child) => {
+/**
+ * Finds the children of a specific type on the children props.
+ * Meant to be used with dot notation components.
+ * @param {any} children the children collection
+ * @param {Function} type the function that represents the component type to find
+ * @returns {ReactNode[]} the new array with the filtered children or an empty array if no child matches the type
+ */
+export function findChildren(children: any, type: Function): ReactNode[] {
+  const newChildren: ReactNode[] = React.Children.map(children, (child) => {
     if (child?.type === type) {
       return child;
     }
--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,22 +1,12 @@
 import { useState } from 'react';
-import { MdLightMode, MdDarkMode, MdVisibility, MdVisibilityOff } from 'react-icons/md';
+import { MdVisibility, MdVisibilityOff } from 'react-icons/md';
 import { useTitle } from 'hooks/use-title';
-import { useTheme } from 'hooks/use-theme';
 import FormField from 'components/form-field/form-field';
 import IconButton from 'components/icon-button/icon-button';
 
 export default function Home() {
   useTitle('Simplr View • Vendas');
-  const { theme, setTheme } = useTheme();
   const [showPassword, setShowPassword] = useState(false);
-
-  function toggleTheme() {
-    if (theme.theme === 'dark') {
-      setTheme({ theme: 'light' });
-    } else if (theme.theme === 'light') {
-      setTheme({ theme: 'dark' });
-    }
-  }
 
   return (
     <main>
@@ -30,10 +20,6 @@ export default function Home() {
           </IconButton>
         </FormField.Icon>
       </FormField>
-      <IconButton onClick={toggleTheme}>
-        {theme.theme === 'light' && <MdLightMode fontSize="2.4rem" />}
-        {theme.theme === 'dark' && <MdDarkMode fontSize="2.4rem" />}
-      </IconButton>
     </main>
   );
 }
